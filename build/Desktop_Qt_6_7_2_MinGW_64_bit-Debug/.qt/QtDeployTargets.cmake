f61663e2ae0188cae61cc1fc8cd80c4ef31691c4
homayun-chaab <homayun.chaab>
2024-08-19 01:54:45 +0330

    initial commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Pharmacy_FILE C:/Users/Homayoun/Documents/Pharmacy/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/Pharmacy.exe)
+set(__QT_DEPLOY_TARGET_Pharmacy_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Pharmacy_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
