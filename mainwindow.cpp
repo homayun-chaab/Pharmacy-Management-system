c0e3dc28eb231378123349d44befb9b8a3f6b54f
homayun-chaab <homayun.chaab>
2024-08-19 11:16:07 +0330

    push button added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,35 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include <QString>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(clicked()),
+            this, SLOT(addFun()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+QStringList medicines;
+QStringList prices;
+QStringList totalQuant;
+
+void MainWindow::addFun()
+{
+    QString medicine = ui->lineEdit->text();
+    QString price = ui->lineEdit_2->text();
+    QString quant = ui->lineEdit_2->text();
+
+    medicines.append(medicine);
+    prices.append(price);
+    totalQuant.append(quant);
+
+
+
+}
+
